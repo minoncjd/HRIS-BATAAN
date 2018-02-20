2560380abb65027b6308e92698c3a3d697889146
Admin <Admin@CSENO>
2018-02-20 14:24:03 +0800

    test

--- HRiS/PrintDTR.xaml.cs
+++ HRiS/PrintDTR.xaml.cs
@@ -50,7 +50,7 @@ namespace HRiS
 
         private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            //LoadComboBox();
+            LoadComboBox();
           
             cbEmployee.IsEnabled = false;
             cbDepartment.IsEnabled = false;
@@ -64,22 +64,22 @@ namespace HRiS
             {
                 using (var db = new LetranIntegratedSystemEntities())
                 {
-                    EList = new List<HRiSClass.EmpCombo>();
+                    //EList = new List<HRiSClass.EmpCombo>();
 
-                    var emp = db.Employees.Where(m => m.Archive == false).ToList();
+                    //var emp = db.Employees.Where(m => m.Archive == false).ToList();
 
-                    foreach (var i in emp)
-                    {
-                        HRiSClass.EmpCombo ec = new HRiSClass.EmpCombo();
-                        ec.EmployeeID = i.EmployeeID;
-                        ec.EmployeeNumber = i.EmployeeNo;
-                        ec.EmployeeName = i.LastName.ToUpper() + ", " + i.FirstName.ToUpper();
-                        EList.Add(ec);
-                    }
+                    //foreach (var i in emp)
+                    //{
+                    //    HRiSClass.EmpCombo ec = new HRiSClass.EmpCombo();
+                    //    ec.EmployeeID = i.EmployeeID;
+                    //    ec.EmployeeNumber = i.EmployeeNo;
+                    //    ec.EmployeeName = i.LastName.ToUpper() + ", " + i.FirstName.ToUpper();
+                    //    EList.Add(ec);
+                    //}
 
-                    cbEmployee.ItemsSource = EList.OrderBy(m => m.EmployeeName);
-                    cbEmployee.DisplayMemberPath = "EmployeeName";
-                    cbEmployee.SelectedValuePath = "EmployeeID";
+                    //cbEmployee.ItemsSource = EList.OrderBy(m => m.EmployeeName);
+                    //cbEmployee.DisplayMemberPath = "EmployeeName";
+                    //cbEmployee.SelectedValuePath = "EmployeeID";
 
                     cbDepartment.ItemsSource = db.AcademicDepartments.OrderBy(m => m.AcaDepartmentName).ToList();
                     cbDepartment.DisplayMemberPath = "AcaDepartmentName";
@@ -124,13 +124,16 @@ namespace HRiS
 
                 if (rbAll.IsChecked == true)
                 {
-                    employees = (from a in db.Employees
-                                 where !(a.bioid == null) && a.Archive == false && a.EmployeeDepartmentID != null
-                                 select a).ToList();
+                    //employees = (from a in db.Employees
+                    //             where !(a.bioid == null) && a.Archive == false && a.EmployeeDepartmentID != null
+                    //             select a).Distinct().ToList();
+
+                    employees = db.Employees.Where(m => m.bioid != null && m.EmployeeDepartmentID != null && m.Archive == false).ToList();
+
                 }
                 else if (rbDepartment.IsChecked == true)
                 {
-                    employees = db.Employees.Where(m => m.EmployeeDepartmentID == deptid && m.EmployeeDepartmentID != null).ToList();
+                    employees = db.Employees.Where(m => m.EmployeeDepartmentID == deptid && m.EmployeeDepartmentID != null && m.bioid != null && m.Archive == false).ToList();
                 }
                 else if (rbEmployeeNumber.IsChecked == true)
                 {
